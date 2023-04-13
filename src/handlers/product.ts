d1ede05dd2cb2f62c2ad7f6c4b9e46e3d0b1dcdf
Yevhen Dovhan <y.d.devbox@gmail.com>
2023-04-13 15:45:15 +0300

    feat: added middleweres to handle varification, added handlers for product entity

---
+++ src/handlers/product.ts
@@ -0,0 +1,67 @@
+import prisma from "../db"
+
+// Get all products related to a user
+export const getProducts = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      products: true,
+    }
+  });
+  res.json({ data: user.products });
+}
+
+// Get one product by ID
+export const getProductById = async (req, res) => {
+  const id = req.params.id;
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+      belongsToId: req.user.id,
+    }
+  });
+  res.json({ data: product });
+}
+
+// Create new product
+export const createProduct = async (req, res) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      belongsToId: req.user.id,
+    }
+  });
+  res.json({ data: product });
+}
+
+// Update single product
+export const updateProductById = async (req, res) => {
+  const updated = await prisma.product.update({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      }
+    },
+    data: {
+      name: req.body.name,
+    }
+  });
+  res.json({ data: updated });
+}
+
+// Delete product by ID
+export const deleteProductById = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    }
+  });
+  res.json({ data: deleted });
+}
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { handleInputsErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -8,24 +10,50 @@ router.get('/product', (req, res) => {
   res.json({ message: "hello from product rout" });
 });
 router.get('/product/:id', () => { });
-router.put('/product/:id', () => { });
-router.post('/product', () => { });
+router.put('/product/:id', [body('name').isString(), handleInputsErrors], (req, res, next) => {
+});
+router.post('/product', [body('name').isString(), handleInputsErrors], (req, res) => {
+});
 router.delete('/product/:id', () => { });
 
 // Update
 
 router.get('/update', () => { });
 router.get('/update/:id', () => { });
-router.put('/update/:id', () => { });
-router.post('/update', () => { });
+
+router.put('/update/:id', [
+  body('title').optional(),
+  body('body').optional(),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+  body('version').optional(),
+  handleInputsErrors,
+], (req, res) => { });
+
+router.post('/update', [
+  body('title').exists().isString(),
+  body('body').exists().isString(),
+  handleInputsErrors,
+], () => { });
+
 router.delete('/update/:id', () => { });
 
 // Update Point
 
 router.get('/updatepoint', () => { });
 router.get('/updatepoint/:id', () => { });
-router.put('/updatepoint/:id', () => { });
-router.post('/updatepoint', () => { });
+
+router.put('/updatepoint/:id', [
+  body('name').optional().isString(),
+  body('description').optional().isString(),
+  handleInputsErrors,
+], () => { });
+
+router.post('/updatepoint', [
+  body('name').isString(),
+  body('description').isString(),
+  body('updateId').exists().isString(),
+  handleInputsErrors,
+], () => { });
 router.delete('/updatepoint/:id', () => { });
 
 export default router;
